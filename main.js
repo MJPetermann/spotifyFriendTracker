6a568e90758f3b7c0a58123fad22a95ea2afff5b
Michael Petermann <mj@petermann.email>
2022-02-01 21:36:28 +0100

    Init

---
+++ main.js
@@ -0,0 +1,27 @@
+const buddyList = require('spotify-buddylist')
+const fs = require('fs')
+const cron = require('node-cron')
+
+
+
+fs.readFile('./config.cfg', 'utf8', async (err, data) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+    var config = await JSON.parse(data);
+    cron.schedule('* * * * *', async () => {
+        console.log(config);
+        const {
+            accessToken
+        } = await buddyList.getWebAccessToken(config["spotify-token"]);
+        const friendActivity = await buddyList.getFriendActivity(accessToken);
+
+        Array.from(friendActivity.friends).forEach(element => {
+            if (element["user"]["name"] == "Felix") {
+                console.log(element.track.name + " from " + element.track.artist.name);
+            
+            }
+        });
+    });
+})
